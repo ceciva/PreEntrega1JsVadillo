7e7a6c561c58415938f629bdc0ae38ca8a6e2523
cecivadillo <cecivadillo@gmail.com>
2023-02-23 17:59:59 -0300

    subida index, js, css e img 1er entrega Js

---
+++ js/libros.js
@@ -0,0 +1,62 @@
+let libro1 = 5500
+let libro2 = 5500
+let libro3 = 5500
+let libro4 = 4800
+let libro5 = 4200
+let libro6 = 4500
+let libro7 = 6800
+let libro8 = 6200
+let continuar = true
+
+function preguntarPrecios(){
+    while (continuar){
+        iniciarConsulta()
+        continuar = confirm("Querés consultar el precio de otro libro?")
+    }
+}
+
+function iniciarConsulta(){
+    let libroElegido = parseInt(prompt("Ingrese el número del libro que desea comprar?"))
+    if (libroElegido >8 ||libroElegido <1 ){
+        alert('Ingresaste un nro de libro incorrecto')
+        return
+    }
+        switch(libroElegido){
+            case 1:
+                msjCosto = "Caballo de fuego Paris cuesta: $ " + libro1
+                break
+            case 2:
+                msjCosto = "Caballo de fuego Congo cuesta: $ " + libro2
+                break
+            case 3:
+                msjCosto = "Caballo de fuego Gaza cuesta: $ " + libro3
+                break
+            case 4:
+                msjCosto = " Cuarto arcano cuesta: $ " + libro4
+                break
+            case 5:
+                msjCosto = "Indias blancas 1 cuesta: $ " + libro5
+                break
+            case 6:
+                msjCosto = "Indias blancas 2 cuesta: $ " + libro6
+                break
+            case 7:
+                msjCosto = "El hechizo del agua cuesta: $ " + libro7
+                break
+            case 8:
+                msjCosto = "Bodas de odio cuesta: $ " + libro8
+                break
+            default:
+                alert('Ingresaste un nro de libro incorrecto')  
+                return          
+        }
+        alert (msjCosto)
+                        
+}
+
+preguntarPrecios()
+
+    
+    
+    
+ 
